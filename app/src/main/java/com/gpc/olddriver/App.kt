48ca3dc9cfdbaceb5f70d2bc7cf861753dea3556
pengcheng.gu <pengcheng.gu@kankanapp.com.cn>
2018-01-29 20:08:22 +0800

    init kotlin android project

---
+++ app/src/main/java/com/gpc/olddriver/App.kt
@@ -0,0 +1,12 @@
+package com.gpc.olddriver
+
+import android.app.Application
+
+/**
+ * Created by gupengcheng on 2018/1/29.
+ */
+class App :Application(){
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/gpc/olddriver/activity/FlashActivity.kt
@@ -0,0 +1,14 @@
+package com.gpc.olddriver.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.gpc.olddriver.R
+
+class FlashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flash)
+    }
+
+}
